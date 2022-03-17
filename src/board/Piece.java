be4117dca59556dfd8274dfe2c156075b9645ba7
emelinepg <emelinedev84@gmail.com>
2022-03-17 15:02:34 -0300

    Possible moves of a piece

--- src/board/Piece.java
+++ src/board/Piece.java
@@ -1,6 +1,6 @@
 package board;
 
-public class Piece {
+public abstract class Piece {
 	
 	// é protected pois ela não deve ser visível na camada de xadrez
 	protected Position position; 
@@ -16,5 +16,26 @@ public class Piece {
 
 	protected Board getBoard() {
 		return board;
+	}
+	
+	public abstract boolean[][] possibleMoves();
+	
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		//if (mat == null) {
+		//	return false;
+		//}
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
